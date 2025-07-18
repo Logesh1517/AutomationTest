09619ca6bb506a2a2bdf5cba61d7a349e8f2aa8a
Logesh <107234214+Logesh1517@users.noreply.github.com>
2025-07-18 12:28:03 +0530

    Update test01.java

--- test01.java
+++ test01.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class test01 {
 
 	public static void main(String[] args) {
-		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/v1/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-driver.findElement(By.className("btn_action")).click();
+		ChromeDriver driver = new ChromeDriver(); // creating object for chrome driver
+		driver.get("https://www.saucedemo.com/v1/"); // opening the web page using the url provided
+		driver.findElement(By.id("user-name")).sendKeys("standard_user"); // enter username by using unique id value in username script
+		driver.findElement(By.id("password")).sendKeys("secret_sauce"); // enter password by using unique id value in password script
+driver.findElement(By.className("btn_action")).click(); // use click function to login by using unique classname in login script
 	}
 
 }
